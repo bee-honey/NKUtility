349053130b23a4aabb00ac093c314ebe05ad6f30
bee-honey <honeybeeprogrammer@gmail.com>
2024-06-14 20:10:44 -0700

    Initial Commit

---
+++ Sources/NKUtility/Array+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Naveen Keerthy on 6/14/24.
+//
+
+import Foundation
+
+public extension Array {
+    subscript(safeIndex index: Int) -> Iterator.Element? {
+        return index >= 0 && index < endIndex ? self[index] : nil
+    }
+}
---
+++ Tests/NKUtilityTests/Array+ExtensionsTests.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  
+//
+//  Created by Naveen Keerthy on 6/14/24.
+//
+
+import XCTest
+
+final class Array_ExtensionsTests: XCTestCase {
+    
+    func test_safeIndex_withValidIndex() {
+        let numbers = [1,2,3,4,5]
+        let num1 = numbers[safeIndex: 2]
+        XCTAssertNotNil(num1)
+    }
+    
+    func test_safeIndex_withNilValue() {
+        let numbers = [1,2,3,4,5]
+        let num1 = numbers[safeIndex: 10]
+        XCTAssertNil(num1)
+    }
+    
+}
